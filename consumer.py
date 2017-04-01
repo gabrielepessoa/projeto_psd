170748bed64e9924a347fa16e7d834093d05ac09
Gabriele Pessoa <gabriele.pessoa@gmail.com>
2017-04-01 12:59:50 -0300

    Códigos Atualizados

--- consumer.py
+++ consumer.py
@@ -1,12 +1,12 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.mqtt import MQTTUtils
-import requests
-
 
 sc = SparkContext()
 ssc = StreamingContext(sc, 1)
 
+Dennis_Dotterweich = {'1:1':0,'1:2':0,'1:3':0,'1:4':0,'1:5':0,'1:6':0,'1:7':0,'1:8':0,'1:9':0,'1:10':0,'1:11':0,'1:12':0,'1:13':0,'2:1':0,'2:2':0,'2:3':0,'2:4':0,'2:5'$
+'6:2':0,'6:3':0,'6:4':0,'6:5':0,'6:6':0,'6:7':0,'6:8':0,'6:9':0,'6:10':0,'6:11':0,'6:12':0,'6:13':0,'7:1':0,'7:2':0,'7:3':0,'7:4':0,'7:5':0,'7:6':0,'7:7':0,'7:8':0,'7:$
 
 def posicao_matriz(valores_fila):
         pos = valores_fila.split(".")
@@ -14,14 +14,14 @@ def posicao_matriz(valores_fila):
         px = int(x/5225)
         py = int((33965 -y)/(6560.375))
         pos_matriz = (str(px)+":"+str(py))
-        #print("x : "+str(x) + " y : " + str(y))
-        response = requests.get("http://192.168.25.25:5000/add/time_a/Dennis_Dotterweich/" + pos_matriz) #ipdeondeestarodandoowebservice
-        print ("http://192.168.25.25:5000/add/time_a/Dennis_Dotterweich/"+ pos_matriz)
-        print response.content
+        print("x : "+str(x) + " y : " + str(y))
+        try:
+                Dennis_Dotterweich[pos_matriz] = Dennis_Dotterweich[pos_matriz] + 1
+        except:
+                print("except")
+                Dennis_Dotterweich['0:0'] = Dennis_Dotterweich['0:0'] + 1
 
         return ("Posicao da matriz: " + pos_matriz)
-
-
 mqttStream = MQTTUtils.createStream(#DStream
     ssc,
     "tcp://localhost:1883",  # Note both port number and protocol
@@ -31,8 +31,7 @@ mqttStream = MQTTUtils.createStream(#DStream
 mqttStream.map(posicao_matriz).pprint()
 
 ssc.start()
-ssc.awaitTermination()
+ssc.awaitTermination(10)
 ssc.stop()
 
-print max(Dennis_Dotterweich, key=Dennis_Dotterweich.get)
-print ("fora ="+str(Dennis_Dotterweich['0:0']))
+print max(Dennis_Dotterweich, key=Dennis_Dotterweich.ge
